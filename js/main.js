881fdfa649d03b1541845ebba9966ab4e9e21aa1
salgadokevin <115896779+salgadokevin@users.noreply.github.com>
2022-11-24 10:19:52 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function cartel() {alert("Desarrollado por Kevin Salgado - Correo: salgadokevingonzalo@gmail.com - Teléfono: +5492914471522")}
+
+function cambiarModo() {
+    var cuerpoweb = document.body;
+    cuerpoweb.classList.toggle("oscuro");
+}
